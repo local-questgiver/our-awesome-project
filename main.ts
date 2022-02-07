ecfe1da750982383c72a79c44a086598a9434833
KaylaRhae <99204728+KaylaRhae@users.noreply.github.com>
2022-02-07 12:25:18 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,2 +1,19 @@
-let mySprite: Sprite = null
+let mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . e e e . . . . . . . 
+    . . . . . e e e e e . . . . . . 
+    . . . . . e d d d e . . . . . . 
+    . . . . . . d d d . . . . . . . 
+    . . . . . . d d d . . . . . . . 
+    . . . . . . d d d . . . . . . . 
+    . . . . . d d d d d . . . . . . 
+    . . . d d d f d f d d d . . . . 
+    . . . f d f d d d d f d . . . . 
+    . . . d d d d . d d d d . . . . 
+    . . . . d d . . . d d . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
 mySprite.sayText("balls")
