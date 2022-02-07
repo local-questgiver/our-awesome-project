49b6ce5e0fe2e3178995468bb9ad3dd32382d7f7
KaylaRhae <99204728+KaylaRhae@users.noreply.github.com>
2022-02-07 12:27:56 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,18 @@
 let mySprite = sprites.create(img`
     . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . e e e . . . . . . . 
-    . . . . . e e e e e . . . . . . 
-    . . . . . e d d d e . . . . . . 
-    . . . . . . d d d . . . . . . . 
-    . . . . . . d d d . . . . . . . 
-    . . . . . . d d d . . . . . . . 
-    . . . . . d d d d d . . . . . . 
-    . . . d d d f d f d d d . . . . 
-    . . . f d f d d d d f d . . . . 
-    . . . d d d d . d d d d . . . . 
-    . . . . d d . . . d d . . . . . 
-    . . . . . . . . . . . . . . . . 
+    . . . . . . f f f . . . . . . . 
+    . . . . . f 3 3 3 f . . . . . . 
+    . . . . f 3 3 3 3 3 f . . . . . 
+    . . . . f 3 d d d 3 f . . . . . 
+    . . . . . f d d d f . . . . . . 
+    . . . . . f d d d f . . . . . . 
+    . . . . . f d d d f . . . . . . 
+    . . . f f d d d d d f f . . . . 
+    . . f d d d d d d d d d f . . . 
+    . . f d d d d d d d d d f . . . 
+    . . f d d d d f d d d d f . . . 
+    . . . f d d f . f d d f . . . . 
+    . . . . f f . . . f f . . . . . 
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
