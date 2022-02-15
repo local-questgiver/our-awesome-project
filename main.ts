cec8794cb9f6283212b075a30379e288be968040
Gabe <99204919+GNF500@users.noreply.github.com>
2022-02-15 12:09:15 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,18 @@
-
+let mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . f f f f f f f f f f f f f f . 
+    . f 1 1 1 1 1 1 1 1 1 1 1 1 f . 
+    . f 1 1 1 1 1 1 1 1 1 1 1 1 f . 
+    . f 1 1 1 1 1 1 1 1 1 1 1 1 f . 
+    . f 1 1 1 1 1 1 1 1 1 1 1 1 f . 
+    . f 1 1 1 1 1 f f f 1 1 1 1 f . 
+    . f 1 1 1 1 1 f f f 1 1 1 1 f . 
+    . f 1 1 1 1 1 f f f 1 1 1 1 f . 
+    . f 1 1 1 1 1 1 1 1 1 1 1 1 f . 
+    . f 1 1 1 1 1 1 1 1 1 1 1 1 f . 
+    . f 1 1 1 1 1 1 1 1 1 1 1 1 f . 
+    . f 1 1 1 1 1 1 1 1 1 1 1 1 f . 
+    . f 1 1 1 1 1 1 1 1 1 1 1 1 f . 
+    . f f f f f f f f f f f f f f . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
